326ddb5482693e89e33dfc194b149e58ce0e2dcb
anush <anush@DESKTOP-I38AU2U>
2022-08-11 17:45:40 +0530

    first commit

---
+++ hello/src/hello/ArithmaticOperators.java
@@ -0,0 +1,13 @@
+package hello;
+
+public class ArithmaticOperators {
+
+	public static void main(String[] args) {
+		int x = 10, y=20;
+		System.out.println(x+y);
+		System.out.println(--x);
+		System.out.println(++y);
+
+	}
+
+}
---
+++ hello/src/hello/Typecasting.java
@@ -0,0 +1,11 @@
+package hello;
+
+public class Typecasting {
+
+	public static void main(String[] args) {
+	       float f = 10.532f;
+	       long l = (long) f;
+	       System.out.println(l);
+	}
+
+}
